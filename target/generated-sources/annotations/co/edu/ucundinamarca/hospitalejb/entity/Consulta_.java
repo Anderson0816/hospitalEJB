4a011779a1dcd64536f4c696d02f00ad3bef0aab
Anderson Nevao <anderson.nevao810.4@hotmail.com>
2021-04-22 15:23:56 -0500

    cambio de visibilidad
    cambio

--- target/generated-sources/annotations/co/edu/ucundinamarca/hospitalejb/entity/Consulta_.java
+++ target/generated-sources/annotations/co/edu/ucundinamarca/hospitalejb/entity/Consulta_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-21T20:40:36")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-22T14:39:44")
 @StaticMetamodel(Consulta.class)
 public class Consulta_ { 
 
