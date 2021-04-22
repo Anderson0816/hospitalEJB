9d2e3b26beb48e395247dc1153163cfc37491d70
Anderson Nevao <anderson.nevao810.4@hotmail.com>
2021-04-22 10:51:27 -0500

    inicio repo
    changes

---
+++ target/generated-sources/annotations/co/edu/ucundinamarca/hospitalejb/entity/Administrativo_.java
@@ -0,0 +1,17 @@
+package co.edu.ucundinamarca.hospitalejb.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-21T20:40:36")
+@StaticMetamodel(Administrativo.class)
+public class Administrativo_ { 
+
+    public static volatile SingularAttribute<Administrativo, String> cedula;
+    public static volatile SingularAttribute<Administrativo, String> apellido;
+    public static volatile SingularAttribute<Administrativo, Integer> id;
+    public static volatile SingularAttribute<Administrativo, String> nombre;
+    public static volatile SingularAttribute<Administrativo, Integer> edad;
+
+}
---
+++ target/generated-sources/annotations/co/edu/ucundinamarca/hospitalejb/entity/Consulta_.java
@@ -0,0 +1,19 @@
+package co.edu.ucundinamarca.hospitalejb.entity;
+
+import co.edu.ucundinamarca.hospitalejb.entity.DetalleConsulta;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-21T20:40:36")
+@StaticMetamodel(Consulta.class)
+public class Consulta_ { 
+
+    public static volatile SingularAttribute<Consulta, Date> fecha;
+    public static volatile ListAttribute<Consulta, DetalleConsulta> detalleConsulta;
+    public static volatile SingularAttribute<Consulta, String> nombreMedico;
+    public static volatile SingularAttribute<Consulta, Integer> id;
+
+}
